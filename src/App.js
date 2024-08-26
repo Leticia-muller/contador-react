ace1561712d3457bd3fce0254bd40edd282b516a
Letícia Müller Vieira <lelemuller01@gmail.com>
2024-08-26 14:41:13 -0300

    1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export default function App() {
+
+const [numero,setNumero] = useState(0)
+    
+function aumentar() {
+   setNumero(numero + 1)
+}
+function diminuir() {
+   setNumero(numero - 1)
+}
+function zerar() {
+   setNumero(numero - numero)
+}
+
+return <section>
+    <h1>Contador</h1>
+    <p>{numero}</p>
+    <button onClick={aumentar}>mais</button>
+    <button onClick={diminuir}>menos</button>
+    <button onClick={zerar}>zerar</button>
+    </section>
+}
